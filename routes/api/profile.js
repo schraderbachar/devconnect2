abb9f353197e9da8dd1bd5a231a718cad07e5c29
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-06 02:03:35 -0500

    added delete exp route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -193,5 +193,26 @@ async (req,res)=>{
 
 })
 
+//@route    Delete api/profile/experience/:exp_id
+//@desc     delete profile experience
+//@access   private
+router.delete('/experience/:exp_id', auth, async (req,res)=>{
+    try {
+        const profile = await Profile.findOne({user:req.user.id})
+        //get correct exp to remove
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
+
+        profile.experience.splice(removeIndex, 1)
+        await profile.save()
+
+        res.json(profile)
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+        
+        
+    }
+})
 
 module.exports = router;
