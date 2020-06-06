73251a971b6ccba11ab4247407efba04bf04c7d1
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-06 01:26:00 -0500

    get all profiles and get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -96,4 +96,38 @@ router.post('/',[auth,
     }
 })
 
+//@route    GET api/profile
+//@desc     get all profiles
+//@access   public
+router.get('/', async(req,res) => {
+    try {
+        const profiles = await Profile.find().populate('user',['name','avatar'])
+        res.json(profiles)
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+})
+
+//@route    GET api/profile/user/:user_id
+//@desc     get profile by user id
+//@access   public
+router.get('/user/:user_id', async(req,res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id}).populate('user',['name','avatar'])
+
+        if (!profile) return res.status(400).json({msg: 'Profile not found'})
+        res.json(profile)
+        
+    } catch (err) {
+        console.error(err.message);
+        //when the id doesn't belong to a user- aka not an object id
+        if (err.kind == 'ObjectId')  return res.status(400).json({msg: 'Profile not found'})
+        res.status(500).send('Server error')
+    }
+})
+
+
+
 module.exports = router;
