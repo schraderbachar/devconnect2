a83c440746a70e6b19d0adb87c6deb3e708e0eff
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-05 21:55:34 -0500

    all routes are up and working

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoURI')
+
+
+
+
+//funciton to connect to db
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+     
+        console.log('db connected');
+        
+     } catch (err) {
+         console.error(err);
+         //exit with failure
+         process.exit(1)
+     }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const connectDB = require('./config/db')
+const userRoute = require('./routes/api/users');
+const authRoute = require('./routes/api/auth');
+const profileRoute = require('./routes/api/profile');
+const postsRoute = require('./routes/api/posts');
+
+
 
 const app = express()
 
+//connect DB from db.js file
+connectDB();
 
 
 app.get('/',(req,res) => {
     res.send('api up')
 })
+
+//define routes
+//gets the user's file
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/profile', profileRoute)
+app.use('/api/posts', postsRoute)
 
 const PORT = process.env.PORT || 5000
 
