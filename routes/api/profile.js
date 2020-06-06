170432e07567244541defad5ab1823262174705c
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-06 00:18:07 -0500

    added logic to get logged in users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-//@route    GET api/profile
-//@desc     Test route
-//@access   public
-router.get('/', (req,res) => res.send('profile route'))
+//@route    GET api/profile/me
+//@desc     get current users profile
+//@access   private
+router.get('/me',auth, async (req,res) => {
+    try {
+        //sets the profile to the profile thats attacthed to the id of the user   populate gets the vals in array from user model
+        const profile = await Profile.findOne({ user: req.user.id}).populate(
+            'user',
+            ['name','avatar']
+            )
+
+        //no profile logic
+        if (!profile){
+            return res.status(400).json({msg: 'No profile for this user'})
+        }
+        //if found- send profile
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+        
+    }
+})
 
 module.exports = router;
