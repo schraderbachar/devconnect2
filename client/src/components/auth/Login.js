266e6407e59f82ff36d2c6261e94f74fd7eab14c
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-07 01:50:46 -0500

    register and login fully functional

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,56 @@
-import React from 'react'
-
+import React, {Fragment, useState} from 'react'
+import {Link} from 'react-router-dom'
 const Login = () => {
+
+    const [formData, setFormData]  = useState({
+        
+        email:'',
+        password:''
+       
+    });
+
+    //desturcture so its easier to use each
+    const {email,password} = formData
+//gets the name from the form data and only gets the name because for this one thats waht we want
+    const onChange =  e => setFormData({...formData, [e.target.name]: e.target.value})
+
+    const onSubmit = async e => {
+        e.preventDefault()
+        console.log('Success');
+        
+        
+        
+        
+    }
+
     return (
-        <div>
-            Login
+        <Fragment>
+            <section className="container">
+      <h1 className="large text-primary">Login</h1>
+      <p className="lead"><i className="fas fa-user"></i>Login in to Devconnect</p>
+      <form className="form" onSubmit={e=>onSubmit(e)}>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" value={email} 
+           onChange={e=> onChange(e)}  name="email" required />
+        
         </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password} 
+           onChange={e=> onChange(e)} 
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </section>
+        </Fragment>
     )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useState} from 'react'
-
+import {Link} from 'react-router-dom'
 const Register = () => {
 
     const [formData, setFormData]  = useState({
@@ -12,14 +12,15 @@ const Register = () => {
     //desturcture so its easier to use each
     const {name,email,password, password2} = formData
 //gets the name from the form data and only gets the name because for this one thats waht we want
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+    const onChange =  e => setFormData({...formData, [e.target.name]: e.target.value})
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault()
         if (password !== password2){
             console.log('passwords dont match');
         } else{
-            console.log(formData);
+            console.log('Success');
+            
         }
         
         
@@ -72,7 +73,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </section>
         </Fragment>
