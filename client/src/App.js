6fd832044f539f94541237df5ca241e923c120e7
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-07 01:23:17 -0500

    landing page and nav bar all set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Landing from '../src/components/layout/Landing'
 import Navbar from '../src/components/layout/Navbar'
+import Login from '../src/components/auth/Login'
+import Register from '../src/components/auth/Register'
 import './App.css';
 
+
 const App = () => 
-  
+  <Router>
     <Fragment>
          <Navbar />
-         <Landing />
+         <Route exact path ="/" component={Landing} />
+         <section className="container">
+             <Switch>
+    <Route exact path="/register" component={Register} />
+    <Route exact path="/login" component={Login} />
+             </Switch>
+         </section>
     </Fragment>
-  
+    </Router>
 
 
 export default App;
