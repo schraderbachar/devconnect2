341651553a26212a6f9246e881bd1dc57d23caac
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-07 01:09:52 -0500

    added react setup and got the landing page and nav bar set up

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import Landing from '../src/components/layout/Landing'
+import Navbar from '../src/components/layout/Navbar'
+import './App.css';
+
+const App = () => 
+  
+    <Fragment>
+         <Navbar />
+         <Landing />
+    </Fragment>
+  
+
+
+export default App;
